5ca29f1b7dd73529c6aad02c2274ed28a31ad402
Erik Peldan <erik.peldan@gmail.com>
2019-01-12 21:16:26 +0100

    handle_http_request/1 is doing too much at once
    break out parse_http_request/3 from it

--- http.pl
+++ http.pl
@@ -1,4 +1,3 @@
-
 :- module(http, [handle_connection/1]).
 :- use_module(meta, [ignore_safe/1]).
 
@@ -16,19 +15,26 @@ handle_connection(Socket) :-
 
 
 handle_http_request(StreamPair) :-
-  format("Parsing..~n"),
-  phrase_from_stream(http_request(Method, URL), StreamPair), !,
-  format("Done..~n"),
+  parse_http_request(StreamPair, Method, URL, _Headers),
   format("~s ~s~n", [Method, URL]),
   format(StreamPair, "HTTP/1.1 200 OK~nContent-Length: 0~n~n", []).
 
 
-http_request(Method, URL) -->
+parse_http_request(StreamPair, Method, URL, Headers) :-
+  format("Parsing..~n"),
+  phrase_from_stream(http_request(Method, URL, Headers), StreamPair), !,
+  format("Done..~n").
+
+
+http_request(Method, URL, []) -->
   anything(Method), " ", anything(URL), " ", "HTTP/1.1", newline, rest.
+
 
 newline --> [13, 10] ; [13] ; [10].
 
+
 anything([R]) --> [R].
 anything([R | Anything]) --> [R], anything(Anything).
+
 
 rest(_,[]).
