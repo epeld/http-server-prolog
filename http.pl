b70d0d3430087ba5de0dadae652c1c9884c280a3
Erik Peldan <erik.peldan@gmail.com>
2019-01-14 21:55:43 +0100

    HTTP Headers aren't parsed
    implement HTTP header parsing

--- http.pl
+++ http.pl
@@ -15,8 +15,8 @@ handle_connection(Socket) :-
 
 
 handle_http_request(StreamPair) :-
-  parse_http_request(StreamPair, Method, URL, Version, _Headers),
-  format("~s ~s ~s~n", [Method, URL, Version]),
+  parse_http_request(StreamPair, Method, URL, Version, Headers),
+  format("~s ~s ~s ~w~n", [Method, URL, Version, Headers]),
   format(StreamPair, "HTTP/1.1 200 OK~nContent-Length: 0~n~n", []).
 
 
@@ -26,12 +26,60 @@ parse_http_request(StreamPair, Method, URL, Version, Headers) :-
   format("Done..~n").
 
 
-http_request(Method, URL, Version, []) -->
-  anything(Method), " ", anything(URL), " ", http_version(Version), cr, lf, rest.
+http_request(Method, URL, Version, Headers) -->
+  anything(Method), " ", anything(URL), " ", http_version(Version), cr, lf, headers(Headers), rest.
 
 
 cr --> [13].
 lf --> [10].
+sp --> [32].
+
+crlf --> cr, lf.
+
+
+headers([H | Headers]) -->
+  header(H), crlf,
+  headers(Headers).
+
+headers([]) --> crlf.
+
+
+header(Name : Value) -->
+  header_name(CName), ":", sp, header_value(CValue),
+  {
+    %% string_codes(Name, CName),
+    %% string_codes(Value, CValue)
+    Name = CName,
+    Value = CValue
+  }.
+
+header_name([A | Rest]) -->
+  letter_or_dash(A),
+  header_name1(Rest).
+
+header_name1([A | Rest]) -->
+  letter_or_dash(A),
+  header_name1(Rest).
+
+header_name1([A]) -->
+  letter_or_dash(A).
+
+
+header_value([H | Rest]) -->
+  non_whitespace(H),
+  header_value1(Rest).
+
+header_value1([H | Rest]) -->
+  non_whitespace(H),
+  header_value1(Rest).
+
+header_value1([]) --> [].
+
+non_whitespace(C) -->
+  [C],
+  {
+    between(33, 126, C)
+  }.
 
 
 http_version(Version) -->
@@ -45,6 +93,19 @@ version_number([N | Rest]) -->
 version_number1([N | Rest]) -->
   number_or_dot(N),
   (version_number1(Rest) ; {Rest = []}).
+
+
+letter_or_dash(A) --> letter(A) ; dash(A).
+
+dash(A) --> "-", {[A] = "-"}.
+
+letter(A) -->
+  [A],
+  {
+    member(A, "abcdefghijklmnopqrstuvwxyz") ;
+    member(A, "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+  }.
+
 
 number_or_dot(N) -->
   digit(N).
