ef49ee6caa47de9443c4e9ccbfc9b6f6edf2b7d5
Erik Peldan <erik.peldan@gmail.com>
2019-01-19 11:31:30 +0100

    Meta predicates produce strange errors
    Mark predicates as 'meta' to get better error messages

--- meta.pl
+++ meta.pl
@@ -1,5 +1,8 @@
-
 :- module(meta, [ignore_safe/1, ignore_safe_print/1]).
+
+:- meta_predicate
+    ignore_safe(0),
+    ignore_safe_print(0).
 
 ignore_safe(Goal) :-
   ignore(catch(Goal, _Err, true)).
