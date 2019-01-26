368e74d59abbdc532deb3f4a25c384e8e50189c0
Erik Peldan <erik.peldan@gmail.com>
2019-01-26 12:41:10 +0100

    Hard to understand how url util is to be used
    Add comment explaining use

--- url.pl
+++ url.pl
@@ -1,7 +1,14 @@
-:- module(urls, []).
+:- module(urls, [match_parts/3]).
 
 :- set_prolog_flag(double_quotes, string).
 
+% Expects a "pattern", i.e something like:
+%   ["users", user_id, "profile"]
+% matches that against an URL, e.g
+%   users/foo/profile
+% produces a list of matched parts, i.e
+%   [user_id - "foo"]
+% in this case
 match_parts(Pattern, URL, Matching) :-
   split_string(URL, "/", "", Parts),
   match_parts_rec(Pattern, Parts, Matching).
