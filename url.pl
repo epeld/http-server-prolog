570f109be833c5345a1eb953bbb634c348239518
Erik Peldan <erik.peldan@gmail.com>
2019-01-26 12:53:27 +0100

    Inconvenient to type out full url patterns
    Provide a utility for that

--- url.pl
+++ url.pl
@@ -1,4 +1,4 @@
-:- module(urls, [match_parts/3]).
+:- module(urls, [match_parts/3, pattern_string/2]).
 
 :- set_prolog_flag(double_quotes, string).
 
@@ -21,3 +21,25 @@ match_parts_rec([S | RestPattern], [S | RestParts], Matching) :-
 match_parts_rec([P | RestPattern], [S | RestParts], [ (P-S) | Matching]) :-
   atom(P),
   match_parts_rec(RestPattern, RestParts, Matching).
+
+
+
+% Convenience util for converting a string like
+%   foo/:bar/baz
+% into something that can be passed to match_parts_rec/3, i.e
+%   ["foo", bar, "baz"]
+% in this case
+pattern_string(String, Pattern) :-
+  split_string(String, "/", "", Parts),
+  maplist(pattern_part, Parts, Pattern).
+
+colon_codes(String, Rest) :-
+  string_codes(String, [58 | Rest]).
+
+pattern_part(InString, Atom) :-
+  colon_codes(InString, Codes),
+  atom_string(Atom, Codes).
+
+pattern_part(InString, OutString) :-
+  \+ colon_codes(InString, _),
+  format(string(OutString), "~s", [InString]).
